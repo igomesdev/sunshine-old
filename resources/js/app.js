6fbcfda1f0b70353cba3bd32b96af3af00ad098a
Isidro Gomes <gomezz50@hotmail.com>
2020-06-20 18:15:59 +0100

    working in progress 20-06-20

--- resources/js/app.js
+++ resources/js/app.js
@@ -13,13 +13,13 @@ window.Vue = require('vue');
  * Vue components. It will recursively scan this directory for the Vue
  * components and automatically register them with their "basename".
  *
- * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
+ * Eg. ./components/FollowButton.vue -> <example-component></example-component>
  */
 
 // const files = require.context('./', true, /\.vue$/i)
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+Vue.component('follow-button', require('./components/FollowButton.vue').default);
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
@@ -29,4 +29,9 @@ Vue.component('example-component', require('./components/ExampleComponent.vue').
 
 const app = new Vue({
     el: '#app',
+});
+
+$('#description').on('keyup',function(){
+    var count = $('#description').val().trim().split('');
+    $('#wordCount').text(2000 - count.length);
 });
